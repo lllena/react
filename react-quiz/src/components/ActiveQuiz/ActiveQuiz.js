bcc5bb54b40a375bd693726d871ecdba897a36cf
lllena <lutchencko.lena@gmail.com>
2021-05-07 00:06:48 +0300

    add list question

--- react-quiz/src/components/ActiveQuiz/ActiveQuiz.js
+++ react-quiz/src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./ActiveQuiz.module.css";
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = (props) => (
   <div className={classes.ActiveQuiz}>
@@ -10,12 +11,7 @@ const ActiveQuiz = (props) => (
       </span>
       <small>4/12</small>
     </p>
-    <ul>
-      <li>1</li>
-      <li>4</li>
-      <li>3</li>
-      <li>2</li>
-    </ul>
+        <AnswersList answers={ props.answers}/>
   </div>
 );
 
