d44cb30cfad5b73d661e8f5c4742bae512bec57c
lllena <lutchencko.lena@gmail.com>
2021-05-10 20:15:09 +0300

    add active menu

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,13 +1,22 @@
 import { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
+import { Route, Switch } from "react-router-dom";
 import Quiz from "./containers/Quiz/Quiz";
+import Quizlist from "./containers/QuizList/QuizList";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import Auth from "./containers/Auth/Auth";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={Quizlist} />
+        </Switch>
       </Layout>
     );
   }
