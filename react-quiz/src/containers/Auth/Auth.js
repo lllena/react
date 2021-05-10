8e041cfbe7778931768188032613bb73a7948b7f
lllena <lutchencko.lena@gmail.com>
2021-05-10 23:21:41 +0300

    fix validation

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -10,6 +10,7 @@ function validateEmail(email) {
 
 export default class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: "",
@@ -67,8 +68,13 @@ export default class Auth extends Component {
     control.touched = true;
     control.valid = this.validateControl(control.value, control.validation);
     formControls[controlName] = control;
+    let isFormValid = true;
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
     this.setState({
       formControls,
+      isFormValid,
     });
   };
 
@@ -98,10 +104,18 @@ export default class Auth extends Component {
           <h1>Authorization</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
             {this.renderInputs()}
-            <Button type="success" onClick={this.loginHandler}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Log in
             </Button>
-            <Button type="primary" onClick={this.registerHandler}>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               Create account
             </Button>
           </form>
