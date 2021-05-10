b9b90d4334c8f0082aa8f70b0aa1849cad2d0d87
lllena <lutchencko.lena@gmail.com>
2021-05-10 18:33:06 +0300

    add dinamick tabs

---
+++ pov_2/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Route, NavLink } from "react-router-dom";
+import About from "./About/About";
+import Cars from "./Cars/Cars";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cars">Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        {/*localhost: 3000*/}
+        <Route
+          path="/"
+          exact
+          render={() => <h1 style={{ textAlign: "center" }}>Home page</h1>}
+        />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-hoc/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function mathValue() {
+  return Math.trunc(Math.random() * 20);
+}
+
+function App() {
+  const [counter, setCounter] = useState(() => mathValue());
+  const [state, setState] = useState({
+    title: "Counter",
+    date: Date.now(),
+  });
+
+  function increment() {
+    setCounter((prev) => prev + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
+  function changeTitle() {
+    setState(prev => { return { ...prev, title: 'newTitle'} } )
+  }
+
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button className="btn btn-success" onClick={increment}>
+        Add
+      </button>
+      <button className="btn btn-danger" onClick={decrement}>
+        Del
+      </button>
+      <button className="btn btn-primary" onClick={changeTitle}>
+        Change title
+      </button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App;
