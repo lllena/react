ba781759e4345c954e87ff70688c8f9890640d6b
lllena <lutchencko.lena@gmail.com>
2021-05-12 18:38:50 +0300

    useRef

--- react-hoc/src/App.js
+++ react-hoc/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
- 
+
   return (
     <div>
-     
+      
     </div>
   );
 }
---
+++ react-hoc/src/examples/ref.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function App() {
+  // const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState("initial");
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef("");
+
+  useEffect(() => {
+    renderCount.current++;
+  });
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value]);
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <h1>Counter: {renderCount.current}</h1>
+      <h2>Prev state: {prevValue.current}</h2>
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button className="btn btn-secondary" onClick={focus}>
+        Focus
+      </button>
+    </div>
+  );
+}
+
+export default App;
